2ac15c28003e093e19bb1db769a1e205de63012a
johannino <77020959+johannino@users.noreply.github.com>
2022-06-10 14:06:04 +0200

    Add files via upload

--- kmeans.py
+++ kmeans.py
@@ -3,50 +3,66 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
 from collections import Counter
 import cv2
 
 
+def Kmeans(pictures, clusters):
+    fig = plt.figure()
+    
+    #Indlæser billed
+    img=cv2.imread(pictures)
+    
+    #Indlæser med RGB
+    imgo=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    (h,w,c) = imgo.shape
+    plt.subplot(1, 2, 1)
+    plt.imshow(imgo)
+    
+    #Vektorisere
+    img=imgo.reshape((imgo.shape[1]*imgo.shape[0],3))
+    
+    #Laver K-means på antallet af clusters
+    kmeans=KMeans(n_clusters=clusters, random_state = 1)
+    #Fitter K-means på billedet
+    s=kmeans.fit(img)
+    
+    #Angiver antal labels på antal clusters
+    labels=kmeans.labels_
+    labels=list(labels)
+    
+    #Finder centrum af farver
+    centroid=kmeans.cluster_centers_
+    rgb_cols = kmeans.cluster_centers_.round(0).astype(int)
+    
+    
+    
+    #Finder procentdel af farven i billedet
+    percent=[]
+    for i in range(len(centroid)):
+      j=labels.count(i)
+      j=j/(len(labels))
+      percent.append(j)
+    
+    #
+    plt.subplot(1, 2, 2)
+    plt.bar(np.arange(len(centroid)), percent, color=centroid/255)
+    plt.xlabel("Colors")
+    plt.ylabel("Percentage color")
+    plt.show()
+    
+    
+    #Udregner euklidisk distance mellem antallet af clusters
+    dists = euclidean_distances(kmeans.cluster_centers_)
+    #Tager gennemsnitlig distance af antal clusters
+    tri_dists = dists[np.triu_indices(clusters, 1)]
+    max_dist, avg_dist, min_dist = tri_dists.max(), tri_dists.mean(), tri_dists.min()
+    
+    return max_dist, avg_dist, min_dist
 
-imagename = "/Users/Yohan/Downloads/02466Fagprojekt-main/images/49763410506_729ce62fef_z.jpg"
 
-img=cv2.imread(imagename)
-plt.imshow(img)
-
-#RGB
-img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-
-
-img=img.reshape((img.shape[1]*img.shape[0],3))
-
-kmeans=KMeans(n_clusters=5)
-s=kmeans.fit(img)
-
-
-labels=kmeans.labels_
-print(labels)
-labels=list(labels)
-
-centroid=kmeans.cluster_centers_
-print(centroid)
-
-
-percent=[]
-for i in range(len(centroid)):
-  j=labels.count(i)
-  j=j/(len(labels))
-  percent.append(j)
-print(percent)
-
-
-
-plt.bar(range(len(centroid)), percent, color=centroid/255)
-plt.xlabel("Colors")
-plt.ylabel("Percentage color")
-plt.show()
-
-
+print(Kmeans("/Users/Yohan/Downloads/02466Fagprojekt-main/images/50634283257_54e4759bfb_z.jpg", 5))
 ### optimal K
 """
 md=[]
